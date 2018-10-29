930cc2ca08c546530470dc2e0e6b3d0e8bffbc71
Dave Cook <davecook@hotmail.co.uk>
2018-10-29 15:10:11 -0600

    styling finished

--- src/App.js
+++ src/App.js
@@ -2,314 +2,364 @@ import React from 'react';
 import './App.css';
 
 const oWords = shuffle(require("./words.json"));
-const leftRight = ["left","right"];
-let fallDistance = 20;
+let speed = 500;
+const fallDistance = 1;
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-  return array;
+    return array;
 }
 
-function randomLeft() {
-  return randomIntFromInterval(40,50) + "%";
-}
-function randomIntFromInterval(min,max) // min and max included
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
 
 class Block extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      id:props.id,
-      word:props.word.word,
-      top:props.top,
-      category:props.word.category,
-      left:props.left,
+    constructor(props){
+        super(props);
+        this.state = {
+            id:props.id,
+            word:props.word.word,
+            top:props.top,
+            category:props.word.category,
+            answeredWrong:props.answeredWrong,
+            answeredCorrectly:props.answeredCorrectly,
+        }
     }
-  }
 
+    componentWillReceiveProps(nextProps){
+        if(this.props !== nextProps) {
+            this.setState({
+                top:nextProps.top,
+                selected:nextProps.selected,
+                left:nextProps.left,
+                right:nextProps.right,
+                answeredWrong:nextProps.answeredWrong,
+                answeredCorrectly:nextProps.answeredCorrectly,
+            })
+        }
+
+    }
+
+    createClassList = () => {
+        let classes = "Block ";
+        if (this.state.answeredWrong){
+            classes += "wrong ";
+        }
+        if (this.state.answeredCorrectly){
+            let direction = (this.state.category == "countable" ? "left" : "right");
+            classes += "answered-correctly-" + direction;
+        }
+        return classes;
+    }
+
+
+
+    top = () => {return ((this.state.top - 10) * 5).toString() + "%";}
+
+    render() {
+        return (
+            <div className={this.createClassList()} style={{
+                top:this.top(),
+                left:this.state.left,
+                right:this.state.right,
+            }}>
+                {this.state.word}
+            </div>
+        );
+    }
+}
+
+class Stats extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        answeredCorrectly:props.answeredCorrectly,
+        answeredWrong:props.answeredWrong,
+      }
+  }
   componentWillReceiveProps(nextProps){
       if(this.props !== nextProps) {
           this.setState({
-              top:nextProps.top,
-              selected:nextProps.selected,
+              answeredWrong:nextProps.answeredWrong,
+              answeredCorrectly:nextProps.answeredCorrectly,
           })
       }
 
   }
 
-  createClassList = () => {
-      let classes = "Block ";
-      if(this.state.selected){
-          classes += "shoot-off-" + leftRight[(this.state.id % 2)];
-      }
-      return classes;
-  }
-
-
-
-  top = () => {return ((this.state.top / 25) + 5).toString() + "%";}
-
   render() {
     return (
-      <div className={this.createClassList()} style={{
-          top:this.top(),
-          left:this.state.left,
-        }}>
-        {this.state.word}
+      <div className="stats">
+        <div className="half-width">
+          <span className="stats-text">Answered correctly:</span>
+          <span className="stats-text">Answered wrong:</span>
+          <span className="stats-text">Level:</span>
+        </div>
+        <div className="half-width">
+          <span className="stats-text digit">{this.state.answeredCorrectly}</span>
+          <span className="stats-text digit">{this.state.answeredWrong}</span>
+          <span className="stats-text digit">{this.state.answeredCorrectly}</span>
+        </div>
       </div>
-    );
+    )
   }
 }
 
 class Screen extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-        timer: 0,
-        fallenBlocks:this.props.fallenBlocks,
-        fallingBlock:this.props.fallingBlock,
-        words: this.props.words,
+    constructor(props){
+        super(props);
+        this.state = {
+            timer: 0,
+            fallenBlocks:this.props.fallenBlocks,
+            fallingBlock:this.props.fallingBlock,
+            words: this.props.words,
+        }
+
+    }
+    componentWillReceiveProps(nextProps){
+        if (nextProps!==this.props){
+            this.setState({
+                blocks:nextProps.blocks,
+                fallingBlock:nextProps.fallingBlock,
+                answeredBlocks:nextProps.answeredBlocks,
+                fallenBlocks:nextProps.fallenBlocks,
+            })
+        }
+    }
+    answeredCorrectly = () => {
+      if(!this.state.answeredBlocks){
+        return 0;
+      }
+      return this.state.answeredBlocks.length;
     }
 
-  }
-  componentWillReceiveProps(nextProps){
-    if (nextProps!==this.props){
-      this.setState({
-        blocks:nextProps.blocks,
-        fallingBlock:nextProps.fallingBlock,
-        answeredBlocks:nextProps.answeredBlocks,
-        fallenBlocks:nextProps.fallenBlocks,
-      })
-    }
-  }
+    createFallenBlocks = () => {
+        const fallenBlocks = this.props.fallenBlocks;
+        let resultsArray = [];
+        fallenBlocks.forEach((b) => {
+            const blockElement = (<Block
+                    key={b.key}
+                    top={b.top}
+                    word={b.word}
+                    selected={b.selected}
+                    id={b.key}
+                    left={b.left}
+                    answeredWrong={b.answeredWrong}
+                />
+            );
+            resultsArray.push(blockElement);
+        });
+        return resultsArray;
 
-  createFallenBlocks = () => {
-    const fallenBlocks = this.props.fallenBlocks;
-    let resultsArray = [];
-    fallenBlocks.forEach((b) => {
-      const blockElement = (<Block
-            key={b.key}
-            top={b.top}
-            word={b.word}
-            selected={b.selected}
-            id={b.key}
-            left={b.left}
+    }
+    createFallingBlock = () => {
+        const fallingBlock = this.state.fallingBlock;
+
+        const blockElement = (<Block
+                key={fallingBlock.key}
+                top={fallingBlock.top}
+                word={this.state.words[fallingBlock.key]}
+                selected={fallingBlock.selected}
+                id={fallingBlock.key}
+                left={fallingBlock.left}
+                right={fallingBlock.right}
+                answeredWrong={fallingBlock.answeredWrong}
+                answeredCorrectly={fallingBlock.answeredCorrectly}
             />
         );
-        resultsArray.unshift(blockElement);
-    });
-    return resultsArray;
+        return blockElement;
+    }
 
-  }
-  createFallingBlock = () => {
-    const fallingBlock = this.state.fallingBlock;
-
-    const blockElement = (<Block
-          key={fallingBlock.key}
-          top={fallingBlock.top}
-          word={this.state.words[fallingBlock.key]}
-          selected={fallingBlock.selected}
-          id={fallingBlock.key}
-          left={fallingBlock.left}
-          />
-      );
-    return blockElement;
-  }
-
-  render() {
-    return (
-      <div className="Screen">
-        {this.createFallingBlock()}
-        {this.createFallenBlocks()}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Screen">
+              <div className="block-screen">
+                {this.createFallenBlocks()}
+                {this.createFallingBlock()}
+              </div>
+              <Stats
+                answeredWrong={this.state.fallenBlocks.length}
+                answeredCorrectly={this.answeredCorrectly()}
+                level={Math.floor(this.state.fallingBlock.id / 10) + 1}
+                />
+            </div>
+        );
+    }
 }
 
 class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      side:props.side,
-      category:props.category,
+    constructor(props) {
+        super(props);
+        this.state = {
+            side:props.side,
+            category:props.category,
+        }
     }
-  }
-  createClassList = () => {
-    return `Button ${this.state.side}`
-  }
-  onClick = () => {
-    this.props.choose(this.state.category);
-  }
+    createClassList = () => {
+        return `Button ${this.state.side}`
+    }
+    onClick = () => {
+        this.props.choose(this.state.category);
+    }
 
-  render() {
-    return (
-      <div className= {"btn-holder " + this.state.side} >
-        <div className={this.createClassList()}>
-          <div className="btn-inner" onClick={this.onClick}></div>
-        </div>
-        <span className="btn-tag">{this.state.category}</span>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className= {"btn-holder " + this.state.side} >
+                <div className={this.createClassList()}>
+                    <div className="btn-inner" onClick={this.onClick}></div>
+                </div>
+                <span className="btn-tag">{this.state.category}</span>
+            </div>
+        );
+    }
+}
+
+class FunctionButton extends React.Component {
+}
+
+class PauseButton extends FunctionButton {
+    onClick = () => {
+        this.props.stopTimer();
+    }
+    render() {
+        return (
+            <div className={"btn-holder function-button pause"}>
+                <div className={"function-button pause"} onClick={this.onClick}>
+                </div>
+                <span className="btn-tag func">Pause</span>
+            </div>
+        );
+    }
+}
+class StartButton extends FunctionButton{
+    onClick = () => {
+        this.props.startTimer();
+    }
+    render() {
+        return (
+            <div className={"btn-holder function-button start"}>
+                <div className={"function-button start"} onClick={this.onClick}>
+                </div>
+                <span className="btn-tag func">Start</span>
+            </div>
+        );
+    }
 }
 
 class Game extends React.Component {
-  constructor(props){
-    super(props);
-    this.state ={
-        answered:false,
-        words:oWords,
-        answeredBlocks:[],
-        fallingBlock:
-          {
-              key:0,
-              top:0,
-              selected:false,
-              word:oWords[0],
-              left: randomLeft(),
-          },
-        fallenBlocks:[],
-      //     blocks:[
-      //       {
-      //           key:0,
-      //           top:0,
-      //           selected:false,
-      //           word:oWords[0],
-      //
-      //       }
-      // ],
-      bottom:1600,
-    }
-    this.startTimer = this.startTimer.bind(this);
-      this.startTimer();
+    constructor(props){
+        super(props);
+        this.state ={
+            answered:false,
+            words:oWords,
+            answeredBlocks:[],
+            fallingBlock:
+                {
+                    key:0,
+                    left:0,
+                    right:0,
+                    top:10,
+                    selected:false,
+                    word:oWords[0],
+                    answeredWrong:false,
+                    answeredCorrectly:false,
+                },
+            fallenBlocks:[],
+            bottom:27,
+            timer:true,
+        }
+        this.startTimer = this.startTimer.bind(this);
+        this.startTimer();
     }
 
-/*  addBlock = (lastBlock) => {
-    const currentBottom = this.state.bottom;
-    if (lastBlock.top > currentBottom) return;
-    const newBlock = {
-      key:lastBlock.key + 1,
-      top:0,
-      word:this.state.words[lastBlock.key + 1]
+
+    choose = (cat) => {
+        let currentBlock = this.state.fallingBlock;
+        if (currentBlock.answeredWrong) return;
+        console.log(cat);
+        if (currentBlock.word.category === cat) {
+            this.stopTimer();
+            currentBlock.answeredCorrectly = true;
+            this.setState({
+                fallingBlock:currentBlock,
+            });
+            setTimeout(()=>{
+                currentBlock.selected = true;
+                this.setState({
+                    fallingBlock:currentBlock,
+                    answered:true,
+                });
+                this.startTimer();
+            },300)
+
+
+        } else {
+            currentBlock.answeredWrong = true;
+            this.setState({
+                fallingBlock:currentBlock,
+            });
+        }
     }
-    const blockList = this.state.blocks;
 
-    blockList.push(newBlock);
-    this.setState({
-      blocks:blockList,
-      bottom: currentBottom - 20,
-    });
-  }*/
-
-  choose = (cat) => {
-    const currentBlock = this.state.fallingBlock;
-    if (currentBlock.word.category === cat) {
-        currentBlock.selected = true;
-        this.setState({
-            fallingBlock:currentBlock,
-            answered:true,
-        });
-    }
-  }
-/*
-  dropBlock = (block) => {
-    const currentBottom = this.state.bottom;
-    const blockTop = block.top;
-
-    if (blockTop >= currentBottom) {
-      this.addBlock(block);
-      return block;
-    }
-    block.top += 10;
-    return block;
-  }
-
-  setBlocks = () => {
-      let blocks = this.state.blocks;
-      if(this.state.answered){
-          const lastBlock = blocks[blocks.length - 1];
-          const newBlock = {
-              key:lastBlock.key + 1,
-              top:0,
-              word:this.state.words[lastBlock.key + 1]
-          }
-          setTimeout(() => {
-              if (blocks.length > 1){
-
-                  blocks.pop();
-                  blocks.push(newBlock);
-              } else {
-                  blocks = [newBlock];
-              }
-              this.setState({
-                  blocks:blocks,
-                  answered:false,
-              })
-              const newBlocks = [];
-              for(let i = 0; i < blocks.length;i++) {
-                  let newBlock = this.dropBlock(blocks[i]);
-                  newBlocks.push(newBlock);
-              }
-          },100)
-      } else {
-          blocks = this.state.blocks;
-          const newBlocks = [];
-          for(let i = 0; i < blocks.length;i++) {
-              let newBlock = this.dropBlock(blocks[i]);
-              newBlocks.push(newBlock);
-          }
-          this.setState({blocks:newBlocks});
-      }
-
-  }*/
     dropBlock = (block) => {
         block.top += fallDistance;
         return block;
     }
 
+    lose = () => {
+        this.stopTimer();
+        alert("You lose!");
+    }
+
     setBlocks = () => {
+
         let fallingBlock = this.state.fallingBlock;
         const newBlock = {
             key:fallingBlock.key + 1,
-            top:0,
+            top:10 - Math.floor(-(this.state.bottom - 25)/1.5),
             selected:false,
             word:oWords[fallingBlock.key + 1],
-            left: randomLeft(),
-        }
-        if (fallingBlock.top > this.state.bottom){
-          const nextBottom = this.state.bottom - 136;
-          let fallenBlocks = this.state.fallenBlocks
-          fallenBlocks.push(fallingBlock);
-          this.setState({
-            fallingBlock:newBlock,
-            fallenBlocks:fallenBlocks,
-            bottom:nextBottom,
-          });
-          fallDistance++;
+            right:0,
+            left:0,
+        };
+        const bottom = this.state.bottom;
+        if (fallingBlock.top > bottom){
+            let nextBottom;
+
+            if(this.state.fallenBlocks.length % 3 === 2 && this.state.fallenBlocks.length > 0){
+                nextBottom = this.state.bottom - 3  ;
+            } else {
+                nextBottom = this.state.bottom;
+            }
+
+            let fallenBlocks = this.state.fallenBlocks;
+            fallenBlocks.push(fallingBlock);
+            this.setState({
+                fallingBlock:newBlock,
+                fallenBlocks:fallenBlocks,
+                bottom:nextBottom,
+            });
+            speed -= 10;
+            if (this.state.bottom < 1) {
+                this.lose();
+            }
         }
         if(this.state.answered){
+          let answeredBlocks = this.state.answeredBlocks;
+          answeredBlocks.push(fallingBlock);
             setTimeout(() => {
-
-
-                let answeredBlocks = this.state.answeredBlocks;
-                answeredBlocks.push(fallingBlock);
                 this.setState({
                     fallingBlock:newBlock,
                     answeredBlocks:answeredBlocks,
@@ -325,47 +375,49 @@ class Game extends React.Component {
         fallingBlock = this.state.fallingBlock;
         fallingBlock = this.dropBlock(fallingBlock);
         this.setState({fallingBlock:fallingBlock});
+
     }
 
-    // shootOff = (block) => {
-    //   let counter = 0;
-    //   while (counter > 10){
-    //     block.
-    //   }
-    // }
-
-    startTimer() {
-        this.timer = setInterval(this.setBlocks, 50);
+    startTimer = () => {
+        this.setState({timer:true});
+        this.timer = setInterval(this.setBlocks, speed);
+    }
+    stopTimer = () => {
+        this.setState({timer:false});
+        clearInterval(this.timer);
     }
 
     render() {
         return (
-          <div className="Game">
-          <header className="App-header">
-            Countable/Uncountable
-          </header>
-          <Screen
-            fallenBlocks={this.state.fallenBlocks}
-            fallingBlock={this.state.fallingBlock}
-            words={this.state.words}
-            setBlocks={this.setBlocks}
-             />
-           <Button side="left" category="countable" choose={this.choose}  />
-           <Button side="right" category="uncountable" choose={this.choose} />
-          </div>
-    );
+            <div className="Game">
+                <header className="App-header">
+                    Countable/Uncountable
+                </header>
+                <Screen
+                    answeredBlocks={this.state.answeredBlocks}
+                    fallenBlocks={this.state.fallenBlocks}
+                    fallingBlock={this.state.fallingBlock}
+                    words={this.state.words}
+                    setBlocks={this.setBlocks}
+                />
+                <Button side="left" category="countable" choose={this.choose}  />
+                <PauseButton stopTimer={this.stopTimer}/>
+                <StartButton startTimer={this.startTimer}/>
+                <Button side="right" category="uncountable" choose={this.choose} />
+            </div>
+        );
     }
 }
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
+    render() {
+        return (
+            <div className="App">
 
-        <Game />
-      </div>
-    );
-  }
+                <Game />
+            </div>
+        );
+    }
 }
 
 export default App;
